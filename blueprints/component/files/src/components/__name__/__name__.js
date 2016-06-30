cc3a0799c98d9b8a08eea4a593887abbe5895692
JounQin <liujiaren1992@vip.qq.com>
2016-06-30 21:39:28 +0800

    first blood

--- blueprints/component/files/src/components/__name__/__name__.js
+++ blueprints/component/files/src/components/__name__/__name__.js
@@ -2,9 +2,10 @@ import React from 'react'
 import classes from './<%= pascalEntityName %>.scss'
 
 export const <%= pascalEntityName %> = () => (
-  <div className={classes['<%= pascalEntityName %>']}>
-    <h1><%= pascalEntityName %></h1>
-  </div>
+<div className={classes['<%= pascalEntityName %>']}>
+    <h1>
+        <%= pascalEntityName %></h1>
+</div>
 )
 
 export default <%= pascalEntityName %>
